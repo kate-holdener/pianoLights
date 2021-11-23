8b0558720caf0f9add9e7bb25231cab2f5196155
Kate Holdener <kate.holdener@slu.edu>
2021-11-23 11:30:30 -0600

    Initialization of the lights array is now in the LightsMode constructor

--- lights/SimpleMode.py
+++ lights/SimpleMode.py
@@ -1,15 +1,10 @@
-import board
-import neopixel
 from .lightsMode import LightsMode
 
 class SimpleMode(LightsMode):
     def __init__(self, numLights, colorOn, colorOff):
-        super()
-        self.numLights = numLights
+        super(numLights)
         self.on = colorOn
         self.off = colorOff
-        self.pixels = neopixel.NeoPixel(board.D18, numLights, brightness=1, pixel_order=neopixel.RGB)
-        self.pixels.fill((0,0,0))
 
     def processEvent(self, event):
         message, deltatime = event
--- lights/lightsMode.py
+++ lights/lightsMode.py
@@ -1,3 +1,11 @@
+import board
+import neopixel
+
 class LightsMode:
+    def __init__(self, numLights, pixelOrder=neopixel.RGB):
+        self.pixels = neopixel.NeoPixel(board.D18, numLights, brightness=1, pixel_order=pixelOrder)
+        self.pixels.fill((0,0,0))
+        self.numLights = numLights
+
     def processEvent(self, event):
         pass
