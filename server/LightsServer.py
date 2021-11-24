38cbf6b45c177f4ffd3335d80f4e42728e947963
Kate Holdener <kate.holdener@slu.edu>
2021-11-24 16:36:00 -0600

    added a lights server

--- server/LightsServer.py
+++ server/LightsServer.py
@@ -1,7 +1,9 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse
+import urllib
+from lights.SimpleMode import SimpleMode
 
-
-class LightsServer(BaseHTTPRequestHandler):
+class LightsRequestHandler(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -11,6 +13,17 @@ class LightsServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b'Hello, world!')
+        o = urlparse(self.path)
+        print(o)
+        #if self.path == '/lights':
+        #    do_lights(self)
+        qs = urllib.parse.parse_qs(o.query)
+        print(qs)
+        #imsi = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('imsi', None)
+        #print(imsi)
+
+    def do_lights(self):
+        pass 
 
     def do_POST(self):
         self._set_headers()
@@ -19,5 +32,15 @@ class LightsServer(BaseHTTPRequestHandler):
         post_body = self.rfile.read(content_len)
         self.wfile.write("received post request:<br>{}".format(post_body))
 
-httpd = HTTPServer(('', 8000), LightsServer)
+class LightsServer(HTTPServer):
+    def __init__(self, hostPort, handler):
+        super().__init__(hostPort, handler)
+        self.mode = None
+
+    def setLightsMode(self, mode):
+        self.mode = mode
+        
+httpd = LightsServer(('', 8000), LightsRequestHandler)
+mode = SimpleMode(50, (255,0,0), (0,0,255))
+httpd.setLightsMode(mode)
 httpd.serve_forever()
